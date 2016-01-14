ac88f69a0c17955243b12c3c39ccf06e998523b1
Mark Perkins <mark@allmarkedup.com>
2016-01-14 16:37:18 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# consolidate-engine
-Generic template engine handler powered by Consolidate.js
+# Consolidate Engine
+
+Generic template engine handler for Fractal, powered by [consolidate.js](https://github.com/tj/consolidate.js)
+
+Unlike the other specific template engine handlers, this one does not (currently) allow for any customisation of the underlying template engine instances.
---
+++ engine.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var consolidate = require('consolidate');
+var path        = require('path');
+var _           = require('lodash');
+
+module.exports = {
+
+    partials: {},
+
+    config: null,
+
+    init: function(config){
+        this.config = config;
+    },
+
+    /**
+     * Register component view templates as partials.
+     * Called every time the component file tree changes.
+     */
+
+    registerViews: function(views) {
+        var self = this;
+        views.forEach(function(view){
+            self.partials[view.handle] = view.path;
+            if (view.alias) {
+                self.partials[view.alias] = view.path;
+            }
+        });
+    },
+
+    /**
+     * Render the component view contents.
+     * More work than it should be because it needs some skanky
+     * partial path rewriting to make them play nicely with consolidate.
+     */
+
+    render: function(str, context, meta) {
+        context.partials = {};
+        var tplPath = meta.path;
+        _.each(this.partials, function(partialPath, partialKey){
+            if (tplPath != partialPath) {
+                var relPath = path.relative(tplPath, partialPath).replace('../', '');
+                var parts = path.parse(relPath);
+                if ( !_.isEmpty(parts.name) && (path.extname(tplPath) == path.extname(partialPath))) {
+                    context.partials[partialKey] = path.join(parts.dir, parts.name);
+                }
+            }
+        });
+        return consolidate[this.config.name](meta.path, context);
+    }
+
+};
